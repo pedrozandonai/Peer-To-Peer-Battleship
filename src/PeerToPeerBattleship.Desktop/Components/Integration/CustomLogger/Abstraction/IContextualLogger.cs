25720ea1d29f0a99ed2598a67e23b362381aa653
Pedro Henrique Zandonai Persch <pedrozandonai10@gmail.com>
2024-11-20 19:14:12 -0300

    fix: Removido integração com aplicação desktop

--- src/PeerToPeerBattleship.Desktop/Components/Integration/CustomLogger/Abstraction/IContextualLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using Serilog;
-
-namespace PeerToPeerBattleship.Desktop.Components.Integration.CustomLogger.Abstraction
-{
-    public interface IContextualLogger<T>
-    {
-        ILogger Logger { get; }
-    }
-}
